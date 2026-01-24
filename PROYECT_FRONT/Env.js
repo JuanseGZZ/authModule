61ade13086e0a7b41fdf38319ddefee537a19bd0
juanse <juanseguzzardilopez@hotmail.com>
2026-01-24 16:45:49 -0300

    planing

--- PROYECT_FRONT/Env.js
+++ PROYECT_FRONT/Env.js
@@ -2,3 +2,8 @@
 
 // url del balanceador
 export const AUTH_BASE_URL = "http://localhost:8081"; 
+
+export const StatefulEnabled = true; // sesiones con timer local, y posibilita endpoints de SF
+export const StatefulTimeSession=10; // minutos dura cada sesion SF
+
+
